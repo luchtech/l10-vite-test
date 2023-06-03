0651bf3985def9223bae2eae54b345782b2e71ed
James Carlo Luchavez <jamescarloluchavez@gmail.com>
2023-06-03 09:27:42 +0000

    Fixed HMR for Vite.

--- l10-vite-test/vite.config.js
+++ l10-vite-test/vite.config.js
@@ -4,13 +4,14 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     server: {
-        https: true,
+        https: false,
         host: '0.0.0.0',
-        port: '3000',
+        port: '8080',
         hmr: {
+            host: 'l10-vite-hmr.dvl.to',
+            clientPort: '443',
+            protocol: 'wss',
             https: true,
-            host: 'l10-vite-websocket.dvl.to',
-            port: '3001'
         }
     },
     plugins: [
